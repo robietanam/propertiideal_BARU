07081be57e8718e0d26df78649c4a3f76db54dbc
altamisatmaja <altamisfattah16@gmail.com>
2024-06-12 13:57:30 +0700

    feat: ui

--- app/Http/Controllers/Pages/WebPagesController.php
+++ app/Http/Controllers/Pages/WebPagesController.php
@@ -7,5 +7,47 @@ use Illuminate\Http\Request;
 
 class WebPagesController extends Controller
 {
-    //
+    public function about(){
+        // TODO
+    }
+
+    public function sell(){
+        // TODO
+    }
+
+    public function buy(){
+        // TODO
+    }
+
+    public function detail_properties(){
+        // TODO
+    }
+
+    public function by_properties(){
+        // TODO
+    }
+
+    public function by_solutions(){
+        // TODO
+    }
+
+    public function detail_solutions(){
+        // TODO
+    }
+
+    public function by_categories_property($slug){
+        // TODO
+    }
+
+    public function by_categories_solutions($slug){
+        // TODO
+    }
+
+    public function copyright(){
+        // TODO
+    }
+
+    public function wishlist(){
+        // TODO
+    }
 }
