d344c6f2054598382e069275ac15cafbd71b136f
altamisatmaja <altamisfattah16@gmail.com>
2024-06-26 19:25:57 +0700

    feat: auth for user and partner

--- app/Http/Controllers/Partner/Auth/AuthPartnerController.php
+++ app/Http/Controllers/Partner/Auth/AuthPartnerController.php
@@ -3,9 +3,43 @@
 namespace App\Http\Controllers\Partner\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
+use Auth;
 use Illuminate\Http\Request;
 
 class AuthPartnerController extends Controller
 {
-    //
+    public function index()
+    {
+        if (Auth::check() && Auth::user()->role == 'Admin') {
+            return redirect()->route('partner.dashboard');
+        }
+
+        return view('partner.pages.auth.login');
+    }
+
+    /**
+     * Handle an incoming authentication request.
+     *
+     * @param  \App\Http\Requests\Auth\LoginRequest  $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function store(LoginRequest $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+            if (Auth::user()->role == 'Partner') {
+                return redirect()->route('partner.dashboard');
+            } else {
+                Auth::guard('web')->logout();
+                return redirect()->route('partner.login')->with('status', 'Anda bukan partner!');
+            }
+        }
+
+        return redirect()->route('admin.partner')->with([
+            'status' => 'Email atau password anda salah!',
+        ]);
+    }
 }
--- app/Http/Controllers/User/Auth/AuthUserController.php
+++ app/Http/Controllers/User/Auth/AuthUserController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers\User\Auth;
 
 use App\Http\Controllers\Controller;
+use Auth;
 use Illuminate\Http\Request;
 
 class AuthUserController extends Controller
 {
     public function index(){
+        if (Auth::check() && Auth::user()->role == 'Customer') {
+            return redirect()->route('partner.dashboard');
+        }
         return view('client.auth.login');
     }
 }
